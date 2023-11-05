c74184a26b95598fc6553c6ff9b9270fb4a0f0aa
felixstaude <felixstaude0112@gmail.com>
2023-11-05 01:25:01 +0100

    added soundcloud support

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import spotipy
-import youtube_dl
 import yt_dlp as youtube_dl
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtube_search import YoutubeSearch
@@ -12,16 +11,13 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def get_tracks_from_playlist(playlist_url):
     playlist_id = playlist_url.split('/')[-1].split('?')[0]
-
     results = sp.playlist(playlist_id)
     tracks = results['tracks']['items']
-
     song_details = []
     for track in tracks:
         song_name = track['track']['name']
         artist_name = track['track']['artists'][0]['name']
         song_details.append(f"{song_name} von {artist_name}")
-    
     return song_details
 
 def get_youtube_url(song_name, artist_name):
@@ -33,27 +29,32 @@ def get_youtube_url(song_name, artist_name):
 
 def download_as_mp3(youtube_url, output_path='downloads'):
     options = {
-    'format': 'bestaudio/best',
-    'extractaudio': True,  # Download nur Audio
-    'outtmpl': 'downloads/%(title)s.%(ext)s',  # Name der Ausgabedatei
-    'noplaylist': True,  # Laden Sie nur das Video und nicht die gesamte Playlist herunter
+        'format': 'bestaudio/best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'noplaylist': True,
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([youtube_url])
 
-playlist_url = input("Playlist URL:")
 
-songs = get_tracks_from_playlist(playlist_url)
+def main():
+    link = input("Bitte geben Sie den Link ein (Spotify/SoundCloud): ")
 
-...
-for song in songs:
-    song_name, artist_name = song.split(" von ")
-    youtube_url = get_youtube_url(song_name, artist_name)
-    if youtube_url:
-        print(f"{song} -> {youtube_url}")
-        download_as_mp3(youtube_url)
+    if "spotify.com" in link:
+        songs = get_tracks_from_playlist(link)
+        for song in songs:
+            song_name, artist_name = song.split(" von ")
+            youtube_url = get_youtube_url(song_name, artist_name)
+            if youtube_url:
+                print(f"{song} -> {youtube_url}")
+                download_as_mp3(youtube_url)
+            else:
+                print(f"{song} -> Keine YouTube-URL gefunden.")
+    elif "soundcloud.com" in link:
+        download_as_mp3(link)
     else:
-        print(f"{song} -> Keine YouTube-URL gefunden.")
+        print("Ungültiger Link oder nicht unterstützte Quelle.")
 
-
+if __name__ == "__main__":
+    main()
